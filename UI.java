b84f439979ef03ddb04a1a41800f02eaf3e58c64
jtpham <janethaopham@gmail.com>
2014-07-18 02:23:55 -0700

    Update UI.java

--- UI.java
+++ UI.java
@@ -1,20 +1,3 @@
-/* 
- * @author Jane Pham
- * 
- * This program creates a user interface that displays the preview images of four movie
- * posters (all obtained via Google Images). When hovered over an image, it displays the
- * movie's title and the image's URL. Each image acts as a button; when pressed,
- * a new window opens and displays the selected image as well as all of its available
- * metadata. When the secondary window (that of which contains the metadata) is closed,
- * the main window (the user interface) remains open, and the user may select any image
- * for as many times as desired until the user exits the main window. 
- * 
- * Written in Java SE 7 using Eclipse SDK
- * Library required: Metadata Extractor version 2.6.4
- * 		   Download: https://code.google.com/p/metadata-extractor/downloads/detail?name=metadata-extractor-2.6.4-src.jar
- * 		   		 	 https://code.google.com/p/metadata-extractor/downloads/detail?name=metadata-extractor-2.6.4.zip
-*/
-
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
